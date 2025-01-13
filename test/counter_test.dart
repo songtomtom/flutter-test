9cb8ddb9a4c38f82533678731db10c74e278a08e
songtomtom <celvincolvumn@gmail.com>
2025-01-13 22:55:17 +0900

    test: Counter 테스트 그룹화 및 감소 테스트 추가

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -2,9 +2,21 @@ import 'package:fluttertest/counter.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Counter value should be incremented', () {
-    final counter = Counter();
-    counter.increment();
-    expect(counter.value, 1);
+  group('Counter', () {
+    test('value should start at 0', () {
+      expect(Counter().value, 0);
+    });
+
+    test('value should be incremented', () {
+      final counter = Counter();
+      counter.increment();
+      expect(counter.value, 1);
+    });
+
+    test('value should be decremented', () {
+      final counter = Counter();
+      counter.decrement();
+      expect(counter.value, -1);
+    });
   });
 }
