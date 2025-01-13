95ba6e061c3a0a8dbf4a94351a491088a44785f4
songtomtom <celvincolvumn@gmail.com>
2025-01-13 22:30:16 +0900

    - 기존 widget_test.dart 삭제 - Counter 클래스 및 관련 테스트 추가 (counter.dart, counter_test.dart) - pubspec.yaml에 test 패키지 추가 - 의존성 업데이트 (pubspec.lock)

---
+++ test/counter_test.dart
@@ -0,0 +1,10 @@
+import 'package:fluttertest/counter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Counter value should be incremented', () {
+    final counter = Counter();
+    counter.increment();
+    expect(counter.value, 1);
+  });
+}
